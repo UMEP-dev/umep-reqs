b5b177b71f684819c3835419d5c6d28422cb383c
biglimp <biglimp@users.noreply.github.com>
2023-04-11 10:34:26 +0200

    Removed version for netCDF4

--- umep-reqs/setup.py
+++ umep-reqs/setup.py
@@ -30,7 +30,7 @@ setup(
         "supy==2022.9.22",  # Replace with actual dependency and version number.
         "numba==0.56.4",
         "jaydebeapi==1.2.3",
-        "netCDF4==1.6.1", #   "dependency2==y.y.y" 
+        "netCDF4", #   "dependency2==y.y.y" 
     ],
     author="UMEP dev team",
     long_description=long_description,
